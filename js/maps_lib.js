1b1b7d3bec1998ae2dd46caf42a18fccba5160f4
Jack Dougherty <jack.dougherty@trincoll.edu>
2015-05-14 12:09:22 -0400

    set zIndex levels for polygon layers 1, 2, 3

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -94,7 +94,8 @@ var MapsLib = {
         select: "geometry"
       },
       styleId: 2,
-      templateId: 2
+      templateId: 2,
+      zIndex: 1
     });
     MapsLib.polygon2 = new google.maps.FusionTablesLayer({
       query: {
@@ -102,7 +103,8 @@ var MapsLib = {
         select: "geometry"
       },
       styleId: 2,
-      templateId: 2
+      templateId: 2,
+      zIndex: 2
     });
     MapsLib.polygon3 = new google.maps.KmlLayer(MapsLib.polygon3URL, {
       preserveViewport: true,
